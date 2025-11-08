ad472f6eee5cfc396902e3f2342c7912c9c5b412
joekirincic <jdkirincic@gmail.com>
2025-11-08 11:36:10 -0500

    Updated README files, and made the  function for the synchronous API identical to the one used by the async APIs.

--- async/R/sync-api-plumber2.R
+++ async/R/sync-api-plumber2.R
@@ -1,14 +1,15 @@
 library(plumber2)
 
-query_penguins <- function(n) {
-  con <- DBI::dbConnect(
-    RPostgres::Postgres(),
-    dbname = Sys.getenv("DB_NAME"),
-    host = Sys.getenv("DB_HOST"),
-    port = Sys.getenv("DB_PORT"),
-    user = Sys.getenv("DB_USER"),
-    password = Sys.getenv("DB_PASSWORD")
-  )
+con <- DBI::dbConnect(
+  RPostgres::Postgres(),
+  dbname = Sys.getenv("DB_NAME"),
+  host = Sys.getenv("DB_HOST"),
+  port = Sys.getenv("DB_PORT"),
+  user = Sys.getenv("DB_USER"),
+  password = Sys.getenv("DB_PASSWORD")
+)
+
+query_penguins <- function(con, n) {
   qry <- glue::glue("SELECT * FROM penguins LIMIT {n}")
   out <- DBI::dbGetQuery(con, qry)
 }
@@ -17,6 +18,6 @@ query_penguins <- function(n) {
 #* @get /penguins
 #* @query n:int
 function(query) {
-  out <- query_penguins(query$n)
+  out <- query_penguins(con, query$n)
   out
 }
